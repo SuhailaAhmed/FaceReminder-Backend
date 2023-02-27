4fb082ce2e40fdf2f7ea35f4c4da6385fb3d5961
Soliman Hesham <soliman.hesham@uniparticle.com>
2023-02-27 11:08:00 +0200

    fix issues related to sending mails

--- Gp_Backend/settings.py
+++ Gp_Backend/settings.py
@@ -174,10 +174,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email Settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'mail.uniparticle.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'gp_backend@uniparticle.com'
+EMAIL_HOST_PASSWORD = 'GP_dev2023'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
