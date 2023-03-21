64dfeeb1ee979ec652de093990c91d0652c396fc
Suhaila Ahmed <suhailaahmedbk@gmail.com>
2023-03-21 12:37:25 +0200

    Update Connection

--- accounts/swagger/connection.py
+++ accounts/swagger/connection.py
@@ -1,6 +1,6 @@
 from drf_yasg2 import openapi
 from drf_yasg2.utils import swagger_auto_schema
-from accounts.views.connection_views import create_connection
+from accounts.views.connection_views import create_connection, update_connection
 
 create_connection_endpoint = swagger_auto_schema(
     method="Post",
@@ -18,3 +18,19 @@ create_connection_endpoint = swagger_auto_schema(
     ),
     responses={200: openapi.Response("Realtive added successfully")},
 )(create_connection)
+
+update_connection_endpoint = swagger_auto_schema(
+    method="Patch",
+    request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            "image": openapi.Schema(type=openapi.TYPE_STRING, description="image of relative"),
+            "name": openapi.Schema(type=openapi.TYPE_STRING, description="name of relative"),
+            "relation": openapi.Schema(type=openapi.TYPE_STRING, description="relation of relative"),
+            "age": openapi.Schema(type=openapi.TYPE_STRING, description="age of relative"),
+            "address": openapi.Schema(type=openapi.TYPE_STRING, description="address of relative"),
+            "biography": openapi.Schema(type=openapi.TYPE_STRING, description="biography of relative"),
+        },
+    ),
+    responses={200: openapi.Response("Realtive updated successfully")},
+)(update_connection)
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,22 +1,29 @@
 from django.urls import path, include
-from accounts.views.auth_views import register, login, logout, forget_password,check_token,set_password
+from accounts.views.auth_views import register, login, logout, forget_password, check_token, set_password
 from accounts.views.profile_views import profiles
-from accounts.views.connection_views import create_connection
-from accounts.swagger.auth import register_endpoint, login_endpoint, logout_endpoint, forger_password_endpoint, set_password_endpoint
+from accounts.views.connection_views import create_connection, update_connection
+from accounts.swagger.auth import (
+    register_endpoint,
+    login_endpoint,
+    logout_endpoint,
+    forger_password_endpoint,
+    set_password_endpoint,
+)
 from accounts.swagger.profile import profiles_endpoint
-from accounts.swagger.connection import create_connection_endpoint
+from accounts.swagger.connection import create_connection_endpoint, update_connection_endpoint
 
 auth_urls = [
-    path('register/', register, name='register'),
-    path('login/', login, name='login'),
-    path('logout/', logout, name='logout'),
-    path('forget-password/', forget_password, name="forget-password" ),
-    path('check-token/<str:token>', check_token, name='check-token'),
-    path('set-password/<str:token>', set_password, name='set-password'),
+    path("register/", register, name="register"),
+    path("login/", login, name="login"),
+    path("logout/", logout, name="logout"),
+    path("forget-password/", forget_password, name="forget-password"),
+    path("check-token/<str:token>", check_token, name="check-token"),
+    path("set-password/<str:token>", set_password, name="set-password"),
 ]
 
 urlpatterns = [
-    path('auth/', include(auth_urls)),
-    path('profiles/',profiles, name='profile'),
-    path('connections/',create_connection, name='create_connection')
+    path("auth/", include(auth_urls)),
+    path("profiles/", profiles, name="profile"),
+    path("connections/", create_connection, name="create_connection"),
+    path("connections/connection_id", update_connection, name="update_connection"),
 ]
