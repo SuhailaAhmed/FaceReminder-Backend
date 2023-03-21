f38eb742457b95c1d2d14db1880fea3eea53d76b
Soliman Hesham <soliman.hesham@uniparticle.com>
2023-03-21 14:30:52 +0200

    fixes

--- accounts/serializers/connection.py
+++ accounts/serializers/connection.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
+from drf_extra_fields.fields import Base64ImageField
 
 from accounts.models.connection import Connection
 
 
 class ConnectionSerializer(serializers.ModelSerializer):
+    image = Base64ImageField()
+
     class Meta:
         model = Connection
         exclude = ["account"]
