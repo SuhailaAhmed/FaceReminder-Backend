a1baacaf7fee93e87cd1cc4d5834450254e05b9f
Suhaila Ahmed <suhailaahmedbk@gmail.com>
2023-07-16 21:57:13 +0300

    Email

--- Gp_Backend/settings.py
+++ Gp_Backend/settings.py
@@ -136,6 +136,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
