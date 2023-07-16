e6fef391eb83ce9f286db2d2cebde8374641f0c6
Suhaila Ahmed <suhailaahmedbk@gmail.com>
2023-07-16 22:41:43 +0300

    Domain

--- Gp_Backend/settings.py
+++ Gp_Backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-q@ice*jdn!yhy962nx+s!m-y@(ymrq&^n^0fnbfr6a3^2*4=2m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = ["52.58.150.200", "127.0.0.1", "192.168.1.8"]
+ALLOWED_HOSTS = ["52.58.150.200", "127.0.0.1", "192.168.1.8", "face-reminder.online"]
 
 
 # Application definition
