38fe0f6a6a142efb1f430c79ddeac4b23a96e172
Suhaila Ahmed <suhailaahmedbk@gmail.com>
2023-07-17 23:17:15 +0300

    Logging

--- accounts/views/image_views.py
+++ accounts/views/image_views.py
@@ -19,7 +19,7 @@ from Gp_Backend.settings import MEDIA_ROOT
 # import the logging library
 import logging
 # Get an instance of a logger
-logger = logging.getLogger("__name__")
+logger = logging.getLogger(__name__)
 
 
 @csrf_exempt
